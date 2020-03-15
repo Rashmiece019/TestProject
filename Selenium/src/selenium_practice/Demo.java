5a180a00ca9555b21129fd34deaff944ef248fb4
RASHMI RANJAN <RASHMI RANJAN@192.168.2.22>
2020-03-15 21:15:07 +0530

    updated to JDK13

--- Selenium/src/selenium_practice/Demo.java
+++ Selenium/src/selenium_practice/Demo.java
@@ -7,14 +7,44 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Demo {
-      
-	public static void main(String[] args) {
-		WebDriver d=new FirefoxDriver();
-		d.get("http://www.facebook.com");
-		System.out.println(d.getTitle().trim());
-		
+	WebDriver driver = null;
+	try{
+		System.setProperty("webdriver.chrome.driver", "src/lib/Browser/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
 
+		driver.get("https://www.flipkart.com/");
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
+		if (expectedTitle.equals(actualTitle)) {
+
+			System.out.println("Both title are same!");
+			System.out.println("Title is :" + actualTitle);
+
+		} else {
+
+			System.err.println("Title does not matched!");
+		}
+
+		WebElement pupup = driver.findElement(By.xpath("//button[@class='_2AkmmA _29YdH8']"));
+		boolean flag = pupup.isDisplayed();
+		if (flag) {
+
+			pupup.click();
+
+		} else {
+
+			System.err.println("login Pupup is not present!");
+		}
+
+		// pass iphone in search text box
+		driver.findElement(By.className("LM6RPg")).sendKeys("iPhone");
+		// click the search button
+		driver.findElement(By.className("vh79eN")).click();
+		Thread.sleep(3000);
+
+	}catch(Exception e)
+	{
+		System.out.println(e);
 	}
-	
-	}
-	
+}
